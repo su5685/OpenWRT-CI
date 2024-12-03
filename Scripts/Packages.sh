d6fdf71597d197fa3cb3c8209248221156bd6c82
su5685 <39041413+su5685@users.noreply.github.com>
2024-12-03 15:14:44 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -40,6 +40,14 @@ UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 UPDATE_PACKAGE "luci-app-wolplus" "VIKINGYFY/packages" "main" "pkg"
 
+UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "main" "pkg"
+UPDATE_PACKAGE "luci-app-lucky" "gdy666/luci-app-lucky" "main"
+
+UPDATE_PACKAGE "pgyvpn" "OrayOS/OpenOray" "main" "pkg"
+UPDATE_PACKAGE "luci-app-pgyvpn" "OrayOS/OpenOray" "main"
+
+
+
 if [[ $WRT_REPO != *"immortalwrt"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
 fi
